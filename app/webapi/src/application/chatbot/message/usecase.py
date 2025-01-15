8212fd731b7776db4e02b31c71f4d9a30c3d6b8c
fumizaki <0238.m.0917@gmail.com>
2025-01-15 19:59:49 +0900

    fix logging

--- app/webapi/src/application/chatbot/message/usecase.py
+++ app/webapi/src/application/chatbot/message/usecase.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from .query import ChatbotMessageQuery
 from .model import CreateChatbotMessageModel
 from src.application.core import Credential
@@ -6,6 +7,7 @@ from src.domain.entity.chatbot_message import ChatbotMessage
 from src.domain.repository.chatbot_message import ChatbotMessageRepository
 from src.infrastructure.database.rdb.transaction import TransactionClient
 from src.infrastructure.llm.txt2txt import LLMTxt2TxtClient, Txt2TxtModel, Txt2TxtLLMMessageRole, Txt2TxtLLMMessage
+from src.infrastructure.logging import JsonLineLoggingClient
 
 class ChatbotMessageUsecase:
     def __init__(
@@ -19,21 +21,37 @@ class ChatbotMessageUsecase:
         self.tx = tx
         self.chatbot_message_query = chatbot_message_query
         self.chatbot_message_repository = chatbot_message_repository
+        self.logger = JsonLineLoggingClient.get_logger(self.__class__.__name__)
 
 
     async def get_all_exec(self, chatbot_id: str) -> AggChatbot:
+        self.logger.info(f"Get All Chatbot Message execution started for account: {self.credential.account_id}")
         try:
             agg_chat_in_db: AggChatbot = self.chatbot_message_query.get_agg_chatbot(chatbot_id, self.credential.account_id)
             return agg_chat_in_db
-        finally:
-            self.tx.close()
+        
+        except HTTPException as http_exc:
+            self.logger.error(f"HTTPException occurred: {http_exc.detail}")
+            raise
+
+        except Exception as exc:
+            self.logger.critical(f"Unexpected error during token execution: {str(exc)}", exc_info=True)
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail='Internal Server Error'
+            )
+
 
 
     async def create_exec(self, params: CreateChatbotMessageModel) -> ChatbotMessage:
+        self.logger.info(f"Create Chatbot Message execution started for account: {self.credential.account_id}")
+
         try:
+            self.logger.info(f"Get messages as context in CHatbot: {params.chatbot_id}")
             # コンテキストとして履歴を取得
             context_in_db: list[ChatbotMessage] = self.chatbot_message_repository.get_latest_list_exclude_deleted(params.chatbot_id, 6)
 
+            self.logger.info(f"Create Message with prompt")
             # DBへ保存
             user_chatbot_message_in_db = self.chatbot_message_repository.create(ChatbotMessage(
                     chatbot_id=params.chatbot_id,
@@ -41,6 +59,7 @@ class ChatbotMessageUsecase:
                     content=params.prompt
                 ))
             
+            self.logger.info(f"Create Message with LLM")
             txt2txt = LLMTxt2TxtClient()
             res = await txt2txt.generate(
                 Txt2TxtModel(
@@ -61,8 +80,15 @@ class ChatbotMessageUsecase:
             
             return assistant_chatbot_message_in_db
             
-        except Exception as e:
-            print(e)
+        except HTTPException as http_exc:
+            self.logger.error(f"HTTPException occurred: {http_exc.detail}")
             self.tx.rollback()
-        finally:
-            self.tx.close()
+            raise
+
+        except Exception as exc:
+            self.logger.critical(f"Unexpected error during token execution: {str(exc)}", exc_info=True)
+            self.tx.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail='Internal Server Error'
+            )
--- app/webapi/src/application/oauth/signup/usecase.py
+++ app/webapi/src/application/oauth/signup/usecase.py
@@ -10,6 +10,8 @@ from src.infrastructure.database.rdb.transaction import TransactionClient
 from src.infrastructure.hashing import HashingClient
 from src.infrastructure.oauth import JWTClient
 from src.infrastructure.email import build_signup_request_content, EmailClient
+from src.infrastructure.logging import JsonLineLoggingClient
+
 
 class OAuthSignupUsecase:
 
@@ -26,17 +28,21 @@ class OAuthSignupUsecase:
         self.tx = tx
         self.account_repository = account_repository
         self.account_secret_repository = account_secret_repository
+        self.logger = JsonLineLoggingClient.get_logger(self.__class__.__name__)
 
 
     def request_exec(self, params: OAuthSignupModel) -> None:
+        self.logger.info(f"Reequest execution started for email: {params.email}")
         
         try:
+            self.logger.info(f"Create Account with unverified email: {params.email}")
             account_in_db: Account = self.account_repository.create(Account(
                 email=params.email
             ))
             hasher = HashingClient()
             hashed = hasher.hash(params.password)
 
+            self.logger.info(f"Account Secret created for account: {account_in_db.id}")
             self.account_secret_repository.create(AccountSecret(
                 account_id=account_in_db.id,
                 password=hashed,
@@ -44,7 +50,7 @@ class OAuthSignupUsecase:
                 stretching=hasher.stretching
             ))
 
-            # TODO: 有効期限を設定したトークンを作成し、メールのcontentに入れる
+            self.logger.debug(f"Verification Token generated for account: {account_in_db.id}")
             verification_token = self.jwt.encode_verification_token(
                 account_in_db.id,
                 self.jwt.verification_token_exp,
@@ -52,28 +58,55 @@ class OAuthSignupUsecase:
             )
 
             # メール送信
+            self.logger.info(f"Sending Email for account verification: {account_in_db.email}")
             self.mailer.send_mail(
                 to_add=[params.email],
                 content=build_signup_request_content(verification_token)
             )
 
             self.tx.commit()
-            
-            return
+
         
-        except Exception as e:
+        except HTTPException as http_exc:
+            self.logger.error(f"HTTPException occurred: {http_exc.detail}")
             self.tx.rollback()
+            raise
+
+        except Exception as exc:
+            self.logger.critical(f"Unexpected error during token execution: {str(exc)}", exc_info=True)
+            self.tx.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail='Internal Server Error'
+            )
+
+            
 
 
     def verify_exec(self, key: str) -> str:
+        self.logger.info(f"Verify execution started for : xxx")
         try:
             payload = self.jwt.decode_verification_token(key)
 
+            self.logger.debug(f"Verify Account: {payload.sub}")
             account_in_db: Account = self.account_repository.get_exclude_deleted(payload.sub)
+            
+            
             self.account_repository.verify(account_in_db.id, datetime.now())
             self.tx.commit()
+
             return payload.url
 
-        except:
+        except HTTPException as http_exc:
+            self.logger.error(f"HTTPException occurred: {http_exc.detail}")
             self.tx.rollback()
+            raise
+
+        except Exception as exc:
+            self.logger.critical(f"Unexpected error during token execution: {str(exc)}", exc_info=True)
+            self.tx.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail='Internal Server Error'
+            )
         
